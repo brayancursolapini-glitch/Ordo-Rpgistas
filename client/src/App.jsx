15e02ca51a276d081b274beee703f1512016c520
brayancursolapini-glitch <brayancursolapini@gmail.com>
2026-08-10 08:47:50 -0300

    Add ThemeProvider and components to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,14 @@
+import { ThemeProvider } from "./context/ThemeContext";
+import IntroLoader from "./components/Loading/IntroLoader";
+import Home from "./pages/Home/Home";
 
+function App() {
+    return (
+        <ThemeProvider>
+            <IntroLoader />
+            <Home />
+        </ThemeProvider>
+    );
+}
+
+export default App;
