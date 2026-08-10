34a2d1f847642ef19d41a91f0bcb43aa5c39217a
brayancursolapini-glitch <brayancursolapini@gmail.com>
2026-08-10 09:34:27 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,55 @@
+import { AnimatePresence } from "framer-motion";
+
 import { ThemeProvider } from "./context/ThemeContext";
+import { UserProvider } from "./context/UserContext";
+
 import IntroLoader from "./components/Loading/IntroLoader";
+import WelcomeModal from "./components/Welcome/WelcomeModal";
+
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+
+import { useState } from "react";
+
+function AppContent() {
+    const [page, setPage] =
+        useState("home");
+
+    return (
+        <>
+            <IntroLoader />
+
+            {page === "home" && (
+                <>
+                    <Home
+                        onLogin={() =>
+                            setPage("login")
+                        }
+                    />
+
+                    <WelcomeModal />
+                </>
+            )}
+
+            <AnimatePresence mode="wait">
+                {page === "login" && (
+                    <Login
+                        onBack={() =>
+                            setPage("home")
+                        }
+                    />
+                )}
+            </AnimatePresence>
+        </>
+    );
+}
 
 function App() {
     return (
         <ThemeProvider>
-            <IntroLoader />
-            <Home />
+            <UserProvider>
+                <AppContent />
+            </UserProvider>
         </ThemeProvider>
     );
 }
