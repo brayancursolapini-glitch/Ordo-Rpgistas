4b8f359478fdc968fde658045ed3b76d384da63d
brayancursolapini-glitch <brayancursolapini@gmail.com>
2026-08-18 22:57:32 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { UserProvider } from "./context/UserContext";
 import IntroLoader from "./components/Loading/IntroLoader";
 import WelcomeModal from "./components/Welcome/WelcomeModal";
 
-import Home from "./pages/Home/Home";
+import Landing from "./pages/Landing/Landing";
 import Login from "./pages/Login/Login";
 import Cadastro from "./pages/Cadastro/Cadastro";
 
